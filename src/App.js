d0efa817dacbb9a1be2ea6011c9c3f46f27756d6
Farha Lina <farhalin@gmail.com>
2021-09-28 17:54:56 -0400

    test

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import smile from './smile.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={smile} className="App-logo" alt="logo" />
+       
+        {/* <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
